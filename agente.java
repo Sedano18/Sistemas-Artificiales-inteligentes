3c93b0e7fbfb1049d27811780a149713edae5143
Sedano18 <70345390+Sedano18@users.noreply.github.com>
2020-09-10 00:24:22 -0500

    Add files via upload

--- agente.java
+++ agente.java
@@ -1,22 +1,10 @@
-package agentes;
 
+package Agentes;
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
 
-public class agente extends Agent {
-    
+public class agente extends Agent{
     protected void setup(){
-        this.addBehaviour(new Behaviour(){
-            int x=0;
-            public void action(){
-                System.out.println("x = " + x);
-                x+= 1;
-            }
-            public boolean done(){
-                if(x == 100)return true;
-                else return false;
-            }
-        });
-        
+        accion ac = new accion();
+        ac.action();
     }
 }
